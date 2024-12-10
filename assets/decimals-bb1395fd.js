1357929e177d301cd9ed3e46291cc3123a927326
EstherLavender <127221085+EstherLavender@users.noreply.github.com>
2024-12-10 10:58:17 +0000

    Deploying to gh-pages from @ EstherLavender/SuperLeads@95f88eaa24612f73109c8b6a8c7b89019fcc9690 🚀

---
+++ assets/decimals-bb1395fd.js
@@ -0,0 +1,1 @@
+import{cz as a}from"./index-00247e36.js";import{d as e}from"./decimals-90da6ea1.js";async function i(c){return a(()=>e(c),{cacheKey:`${c.contract.chain.id}:${c.contract.address}:decimals`,cacheTime:Number.POSITIVE_INFINITY})}export{i as decimals};
